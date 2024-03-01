7b125e48f67f747202c916246fd1b3b1aea370d8
fardinalam <fardinalam2020@gmail.com>
2024-03-01 12:27:38 +0600

    display done

--- js/app.js
+++ js/app.js
@@ -7,8 +7,26 @@ const loadTemperature = city =>{
     .then(data => displayTemperature(data));
 }
 const displayTemperature = data =>{
-    const temperature = document.getElementById('temperature');
-    console.log(data.main.temp);
-    temperature.innerText = data.main.temp;
+    // const temperature = document.getElementById('temperature');
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    console.log(data);
+    console.log(data.weather[0].main);
+    // console.log(data.main.temp);
+    // temperature.innerText = data.main.temp;
 }
+
+const setInnerTextById = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // Set city
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
+
 loadTemperature('dhaka');
